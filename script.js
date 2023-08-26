716325b2a38447a1fa3ed43a44ee5bab7f431915
Ahmad Sarrawi <ahmadhsarrawi@gmail.com>
2023-08-26 15:58:21 +0300

    3rd push

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-const favoritesSection = document.querySelector(".container");
-console.log(favoritesSection);
+const favoritesBar = document.querySelector(".favorites-bottom-section");
+console.log(favoritesBar);
+document.querySelector(".show-favorites-button").addEventListener("click", () => favoritesBar.classList.toggle("invisible"));
+
+const root = document.querySelector(":root");
+
+const lightColors = ["#ffffff", "#f0f9ff", "#dddddd", "#333333"];
+const darkColors = ["#1a1a1a", "#282828", "#000000", "#ededed"];
+
+const rootProperties = [
+  "--bg_default",
+  "--bg_body",
+  "--lines-color",
+  "--body-text",
+];
+
+function toggleDarkMode() {
+  const rootStyles = getComputedStyle(root);
+  let colors = [];
+  rootStyles.getPropertyValue("--bg_default") === "#ffffff"
+    ? (colors = darkColors)
+    : (colors = lightColors);
+  for (i = 0; i < colors.length; i++) {
+    root.style.setProperty(rootProperties[i], colors[i]);
+    console.log(rootStyles.getPropertyValue(rootProperties[i]));
+  }
+}
+
+document
+  .querySelector(".dark-mode-button")
+  .addEventListener("click", toggleDarkMode);
